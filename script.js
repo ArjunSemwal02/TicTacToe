e7eead02d2208cb1a3b38c055bddf716fc10b7e0
ArjunSemwal02 <109063191+ArjunSemwal02@users.noreply.github.com>
2023-10-03 13:47:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Welcome to My Tic Tac Toe")
+let turn = "X"
+let isgameover = false;
+
+//Function to change player's Turn
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
+}
+
+//Function to check for a win
+const checkwin = ()=>{
+    let boxtext = document.getElementsByClassName('boxText');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')){
+            document.querySelector('.Info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+        }
+
+    })
+}
+
+//Game Logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxText');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            checkwin();
+            if(!isgameover){
+                document.getElementsByClassName("Info")[0].innerText = "Turn for " + turn;
+            }
+
+        }
+    })
+})
