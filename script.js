b71b3131c9570f3ce7211d798014cd9b0bc0a9a8
Arjun <arjunsemwal02@gmail.com>
2024-10-22 23:56:48 +0530

    updated style and script

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//A web app for playing Zero-Cross
 console.log("Welcome to My Tic Tac Toe")
 let turn = "X"
 let isgameover = false;
 
 //Function to change player's Turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
 }
 
 //Function to check for a win
@@ -22,27 +23,24 @@ const checkwin = ()=>{
     ]
 
     wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')){
+        if( ( boxtext[e[0]].innerText === boxtext[e[1]].innerText ) && ( boxtext[e[2]].innerText === boxtext[e[1]].innerText ) && ( boxtext[e[0]].innerText !== '' ) ) {
             document.querySelector('.Info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
-        }
 
+        }
     })
 }
 
 //Game Logic
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach( element => {
     let boxtext = element.querySelector('.boxText');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener( 'click', () => {
+        if( boxtext.innerText === '' ) {
             boxtext.innerText = turn;
             turn = changeTurn();
             checkwin();
-            if(!isgameover){
-                document.getElementsByClassName("Info")[0].innerText = "Turn for " + turn;
-            }
-
+            if(!isgameover)     document.getElementsByClassName("Info")[0].innerText = "Turn for " + turn;
         }
     })
 })
