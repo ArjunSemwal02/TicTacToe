a9c3c2dc06d429d38e63b61d1bcaf367c9e15aa3
Arjun <arjunsemwal02@gmail.com>
2024-10-25 13:18:09 +0530

    updated the layout, style and script

--- script.js
+++ script.js
@@ -9,24 +9,14 @@ const changeTurn = () => {
 }
 
 //Function to check for a win
-const checkwin = ()=>{
+function checkwin() {
     let boxtext = document.getElementsByClassName('boxText');
-    let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
+    let wins = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
 
-    wins.forEach(e =>{
+    wins.forEach(e => {
         if( ( boxtext[e[0]].innerText === boxtext[e[1]].innerText ) && ( boxtext[e[2]].innerText === boxtext[e[1]].innerText ) && ( boxtext[e[0]].innerText !== '' ) ) {
             document.querySelector('.Info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
-
         }
     })
 }
