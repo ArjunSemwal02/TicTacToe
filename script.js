994e7441513262f3d8aca03a0ce59abe9c2f1213
Arjun <arjunsemwal02@gmail.com>
2024-10-25 14:27:29 +0530

    another commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-//A web app for playing Zero-Cross
-console.log("Welcome to My Tic Tac Toe")
+//Play ZeroKatta
+const resetElement = document.getElementById('reset');
+let boxes = document.getElementsByClassName("box");
+let statusElement = document.querySelector('.status');
 let turn = "X"
-let isgameover = false;
+let gameover = false;
 
 //Function to change player's Turn
 const changeTurn = () => {
@@ -10,27 +12,35 @@ const changeTurn = () => {
 
 //Function to check for a win
 function checkwin() {
-    let boxtext = document.getElementsByClassName('boxText');
-    let wins = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
+    let text = document.getElementsByClassName('boxText');
+    let wins = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
 
     wins.forEach(e => {
-        if( ( boxtext[e[0]].innerText === boxtext[e[1]].innerText ) && ( boxtext[e[2]].innerText === boxtext[e[1]].innerText ) && ( boxtext[e[0]].innerText !== '' ) ) {
-            document.querySelector('.Info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
+        if( ( text[e[0]].innerText === text[e[1]].innerText ) && ( text[e[2]].innerText === text[e[1]].innerText ) && ( text[e[0]].innerText !== '' ) ) {
+            statusElement.innerText = text[e[0]].innerText + " Won";
+            gameover = true;
         }
     })
 }
 
 //Game Logic
-let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach( element => {
-    let boxtext = element.querySelector('.boxText');
+    let text = element.querySelector('.boxText');
     element.addEventListener( 'click', () => {
-        if( boxtext.innerText === '' ) {
-            boxtext.innerText = turn;
+        if( text.innerText === '' ) {
+            text.innerText = turn;
             turn = changeTurn();
             checkwin();
-            if(!isgameover)     document.getElementsByClassName("Info")[0].innerText = "Turn for " + turn;
+            if(!gameover)     document.getElementsByClassName("status")[0].innerText = "Turn for " + turn;
         }
     })
 })
+
+
+//Reseting Game
+// const resetGame = () => {
+    resetElement.addEventListener('click', () => {
+        Array.from(boxes).forEach(box => box.innerText = '');
+        statusElement.textContent = 'Play';
+    })
+// }
